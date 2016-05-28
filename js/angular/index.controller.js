cc7b7836e1275aea7744594f76cfcb1f2ef87332
cis321 <care_nalga01@hotmail.com>
2016-05-28 12:57:21 -0500

    new item functionality done :)

--- js/angular/index.controller.js
+++ js/angular/index.controller.js
@@ -1,6 +1,6 @@
 'use strict';
 
-babyShowerApp.controller('IndexController', function ( $scope, $firebaseArray) {
+babyShowerApp.controller('IndexController', function ($scope, $firebaseArray) {
   var ref = new Firebase('https://babyshowerdb.firebaseio.com');
 
   $scope.giftList = $firebaseArray(ref);
@@ -8,6 +8,14 @@ babyShowerApp.controller('IndexController', function ( $scope, $firebaseArray) {
   $scope.addOwner = function (gift, currentOwnerName) {
     gift.owner = currentOwnerName;
     $scope.giftList.$save(gift);
+    window.location.reload();
+  };
+
+  $scope.createItem = function () {
+    $scope.giftList.$add({
+      description: $scope.newItemDescription,
+      owner: $scope.newItemOwnerName
+    });
     window.location.reload();
   };
 });
