dfe16d3ba4cebaac679aa3794c5d74f40c9eb73b
JW Castillo <jwcastillo@gmail.com>
2017-06-14 13:54:34 -0400

    adaptando al baby de zoe

--- js/angular/index.controller.js
+++ js/angular/index.controller.js
@@ -1,17 +1,17 @@
 'use strict';
 
-babyShowerApp.controller('IndexController', function ($scope, $firebaseArray) {
-  var ref = new Firebase('https://babyshowerdb.firebaseio.com');
+babyShowerApp.controller('IndexController', function($scope, $firebaseArray) {
+  var ref = new Firebase('https://zoebabbyshower.firebaseio.com/');
 
   $scope.giftList = $firebaseArray(ref);
 
-  $scope.addOwner = function (gift, currentOwnerName) {
+  $scope.addOwner = function(gift, currentOwnerName) {
     gift.owner = currentOwnerName;
     $scope.giftList.$save(gift);
     window.location.reload();
   };
 
-  $scope.createItem = function () {
+  $scope.createItem = function() {
     $scope.giftList.$add({
       description: $scope.newItemDescription,
       owner: $scope.newItemOwnerName
